263d01db7552eab7440797965c23848a30d0b79f
sota_masuda <sota.masuda@the-lionsdata.com>
2022-11-22 14:20:49 +0900

    add bootstrap / add nav bar to _app.tsx (as common layout)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar, Container } from 'react-bootstrap'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>
+            Hello EC
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+  )
 }
