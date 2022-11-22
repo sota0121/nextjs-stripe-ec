15b95cec054d7dcccc41c4eef6a3e85c84415efe
sota_masuda <sota.masuda@the-lionsdata.com>
2022-11-22 14:00:14 +0900

    update hello world page: add title to head tag

--- pages/hello-world.tsx
+++ pages/hello-world.tsx
@@ -1,6 +1,12 @@
+import Head from "next/head"
+
 export default function HelloWorld() {
     return (
         <div>
+            <Head>
+                <title>Hello World</title>
+                <meta name="description" content="desc for SEO" />
+            </Head>
             <h1>Hello World</h1>
         </div>
     )
